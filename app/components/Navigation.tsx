b9e6dab0bd426cc41d794a4826fbc359741f24b7
David Idol <david.idol@gmail.com>
2025-04-05 10:58:44 -0400

    Add aria labels

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -56,7 +56,7 @@ export default function Navigation({ title }: NavigationProps) {
       </div>
 
       {/* Mobile Menu Button */}
-      <button type="button" className="md:hidden" onClick={() => setMobileMenuOpen(true)}>
+      <button type="button" className="md:hidden" aria-label="Open Menu" onClick={() => setMobileMenuOpen(true)}>
         <BarsIcon className="w-6 h-6 text-[rgb(var(--gray-600))]" />
       </button>
 
@@ -73,7 +73,12 @@ export default function Navigation({ title }: NavigationProps) {
         <DialogPanel className="fixed inset-y-0 right-0 w-full max-w-xs bg-[rgb(var(--background-rgb))] p-6 shadow-xl">
           <div className="flex items-center justify-between mb-8">
             <DialogTitle className="text-xl font-semibold">{title}</DialogTitle>
-            <button type="button" onClick={() => setMobileMenuOpen(false)} className="text-[rgb(var(--gray-600))]">
+            <button
+              type="button"
+              aria-label="Close Menu"
+              onClick={() => setMobileMenuOpen(false)}
+              className="text-[rgb(var(--gray-600))]"
+            >
               <XMarkIcon className="w-6 h-6" />
             </button>
           </div>
