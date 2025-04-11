297337cc9cccaa342a405a8a287b9e1260dfa784
David Idol <david.idol@gmail.com>
2025-04-11 11:08:41 -0400

    Consolidate .d.ts and types.ts files

--- app/content/marketing.yaml.d.ts
+++ app/content/marketing.yaml.d.ts
@@ -1,4 +1,44 @@
-import { Marketing } from './types';
+export interface Feature {
+  title: string;
+  description: string;
+}
+
+export interface FAQQuestion {
+  question: string;
+  answer: string;
+}
+
+export interface Pricing {
+  title: string;
+  description: string;
+  price: number;
+  currency: string;
+  features: string[];
+  volumePricing: string;
+}
+
+export interface FAQ {
+  title: string;
+  questions: FAQQuestion[];
+}
+
+export interface Marketing {
+  title: string;
+  tagline: string;
+  pageTitle: string;
+  hero: {
+    description: string;
+  };
+  embed: {
+    tagline: string;
+  };
+  features: Feature[];
+  pricing: Pricing;
+  faq: FAQ;
+  cta: {
+    description: string;
+  };
+}
 
 declare const content: Marketing;
 export default content;
