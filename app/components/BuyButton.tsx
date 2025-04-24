ae8dab8370d7f421e83630dc0770c803e87f7d09
David Idol <david.idol@gmail.com>
2025-04-23 20:18:53 -0400

    Update domain for LS embed

--- app/components/BuyButton.tsx
+++ app/components/BuyButton.tsx
@@ -1,12 +1,12 @@
-'use client'
+'use client';
 
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
 export default function BuyButton() {
   useEffect(() => {
     // We need to ensure the button is rendered before the script is loaded
-    const script = document.createElement("script");
-    script.src = "https://app.lemonsqueezy.com/js/lemon.js";
+    const script = document.createElement('script');
+    script.src = 'https://app.lemonsqueezy.com/js/lemon.js';
     script.defer = true;
     document.body.appendChild(script);
 
@@ -17,10 +17,10 @@ export default function BuyButton() {
 
   return (
     <a
-      href="https://idol.lemonsqueezy.com/buy/2d451c52-16d0-468c-8b98-0f1a770852a8"
+      href="https://store.hiddenfilecleaner.app/buy/2d451c52-16d0-468c-8b98-0f1a770852a8"
       className="lemonsqueezy-button block bg-gradient text-white px-8 py-4 rounded-full text-lg font-medium hover:opacity-90 transition-opacity w-full mb-6"
     >
       Purchase Now
     </a>
-  )
+  );
 }
